59894614997859dc6d9e25e945bdf28f23e39bef
ritam-g <ritammaty2006@gmail.com>
2026-07-08 22:51:52 +0530

    feat: initialize GraphQL project with Express and Apollo Server setup

---
+++ GRAPH_QL/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
